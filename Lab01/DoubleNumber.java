2df29d8d418e739bcdfee3efdea2466df2432d1a
benut312 <thainq12205@gmail.com>
2026-03-29 02:03:31 +0700

    Add DoubleNumber.java for basic arithmetic operations

---
+++ Lab01/DoubleNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so thu nhat:");
+        double num1 = sc.nextDouble();
+        System.out.println("Nhap so thu hai :");
+        double num2 = sc.nextDouble();
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        System.out.println("num1 - num2 = "+ (num1 - num2));
+        System.out.println("num1 * num2 = " + (num1 * num2));
+        if(num2 != 0 ){
+            System.out.println("num1 / num2 = " + (num1 /  num2));
+        }else{ 
+            System.out.println("Gia tri khong xac dinh");
+        }
+	}
+}
