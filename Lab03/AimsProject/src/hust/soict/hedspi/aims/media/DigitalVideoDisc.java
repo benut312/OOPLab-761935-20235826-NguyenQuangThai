9c64d76a7a263421379b412e3070ec35182c0e94
benut312 <thainq12205@gmail.com>
2026-05-21 00:56:26 +0700

    Rename package dsai to hedspi in Lab03

---
+++ Lab03/AimsProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
@@ -0,0 +1,40 @@
+package hust.soict.hedspi.aims.media;
+
+public class DigitalVideoDisc extends Disc implements Playable {
+    public DigitalVideoDisc() {
+        super();
+    }
+
+    public DigitalVideoDisc(String title) {
+        super(title);
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        super(title, category, null, 0, cost);
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        super(title, category, director, 0, cost);
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super(title, category, director, length, cost);
+    }
+
+    @Override
+    public void play() {
+        if (getLength() <= 0) {
+            System.out.println("Cannot play DVD: " + getTitle());
+            System.out.println("DVD length is non-positive.");
+            return;
+        }
+        System.out.println("Playing DVD: " + getTitle());
+        System.out.println("DVD length: " + getLength());
+    }
+
+    @Override
+    public String toString() {
+        return "DVD - " + getTitle() + " - " + getCategory() + " - " + getDirector()
+                + " - " + getLength() + ": " + getCost() + " $";
+    }
+}
